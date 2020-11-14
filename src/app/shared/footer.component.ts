202b91098d71df9e148688e9628fa667c6b032cf
GLENNBOT\Glenn <enricoespina@hotmail.com>
2020-11-14 09:59:25 -0600

    RHT-04-Non-Tutorial-Cleanup - footer refactoring for using more Angular-based approaches

--- src/app/shared/footer.component.ts
+++ src/app/shared/footer.component.ts
@@ -8,18 +8,28 @@ export class FooterComponent implements OnInit {
 
     constructor() { }
 
-    ngAfterViewInit() {
+    // ngAfterViewInit() {  //OLD SCHOOL way of doing it, without Angualar
+    //     var btnUpArrow = document.getElementById('upArrowButton');
+    //     if (btnUpArrow) {
+    //         btnUpArrow.addEventListener('click', function () {
+    //             window.scroll({
+    //                 top: 0,
+    //                 left: 0,
+    //                 behavior: 'smooth'
+    //             });
+    //         });
+    //     }
+    // }
 
-        var btnUpArrow = document.getElementById('upArrowButton');
-        if (btnUpArrow) {
-            btnUpArrow.addEventListener('click', function () {
-                window.scroll({
-                    top: 0,
-                    left: 0,
-                    behavior: 'smooth'
-                });
-            });
-        }
+    ngAfterViewInit() {}
+
+    upArrowButton_click(){
+        console.log('inside upArrowButton_click');
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
     }
 
     //toggleFooter(e) {
